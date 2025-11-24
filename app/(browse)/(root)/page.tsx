bda05c556fd463a499cf30c5f7f0010e4f1c5037
asdasd <gdd@gmail.com>
2025-11-24 22:41:09 +0500

    home layout

--- app/(browse)/(root)/page.tsx
+++ app/(browse)/(root)/page.tsx
@@ -1,10 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Redo, Sparkles } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function HomePage() {
   return (
-    <div className='grid grid-cols-5 gap-x-4 items-center border rounded-lg p-8 max-lg:gap-4'>
-      <div className='col-span-3 max-md:col-span-5'></div>
-      <div></div>
+    <div className='grid grid-cols-5 gap-x-4 items-center border rounded-lg p-8 max-lg:p-4 bg-gradient-to-tl from-sidebar to-primary/40'>
+      <div className='col-span-3 max-md:col-span-5'><h1 className='text-3xl font-space-grotesk font-semibold'> Welcome to <span className='text-primary'>Hadji</span></h1> <p className='text-muted-foreground leading-5 text-sm'>Hadji - teach you how to code, one step at a time. Learn to code with interactive lessons, coding challanges, and real-world projects, Start your coding journey today!</p>
+        <div className='flex gap-4 mt-4'>
+
+          <Button asChild><Link href={'/courses'} className='rounded-full' size={"lg"}>Explorer Courses</Link></Button>
+          <Button asChild variant={'secondary'} className='rounded-full ' size={"lg"}><Link href={'/projects'}>View Projects</Link></Button>
+        </div>
+      </div>
+      <div className='col-span-2 max-md:col-span-5'>
+        <Sparkles size={200} className='mx-auto max-lg:hidden'/>
+     
+      </div>
     </div>
   )
 }
--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -7,7 +7,7 @@ function Logo() {
         <Link href={'/'}>
             <div className="flex items-center gap-1">
                 <Image src={'/Logo.svg'} width={100} height={100} alt='Logo'/>
-                <div className="text-4xl font-bold font-space-grotesk">Hadji</div>
+                <div className="text-4xl font-bold font-space-grotesk text-primary">Hadji</div>
             </div>
         </Link>
     )
